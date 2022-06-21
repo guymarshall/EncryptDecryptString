cc49b8b10dd6570d5442ab9b9956f2afa4818d56
Guy Marshall <guymarshall9@gmail.com>
2022-06-21 21:50:34 +0100

    Remove using namespace std

--- EncryptDecryptString.cpp
+++ EncryptDecryptString.cpp
@@ -1,20 +1,19 @@
 //Simple C++ program to encrypt and decrypt a string
 
 #include <iostream>
-using namespace std;
 
 int main()
 {
    int i, x;
    char str[100];
 
-   cout << "Please enter a string:\t";
-   cin >> str;
+   std::cout << "Please enter a string:\t";
+   std::cin >> str;
 
-   cout << "\nPlease choose following options:\n";
-   cout << "1 = Encrypt the string.\n";
-   cout << "2 = Decrypt the string.\n";
-   cin >> x;
+   std::cout << "\nPlease choose following options:\n";
+   std::cout << "1 = Encrypt the string.\n";
+   std::cout << "2 = Decrypt the string.\n";
+   std::cin >> x;
 
    //using switch case statements
    switch(x)
@@ -24,7 +23,7 @@ int main()
          for(i = 0; (i < 100 && str[i] != '\0'); i++)
             str[i] = str[i] + 2; //the key for encryption is 3 that is added to ASCII value
 
-         cout << "\nEncrypted string: " << str << endl;
+         std::cout << "\nEncrypted string: " << str << std::endl;
          break;
 
       //second case for decrypting a string
@@ -32,11 +31,11 @@ int main()
          for(i = 0; (i < 100 && str[i] != '\0'); i++)
          str[i] = str[i] - 2; //the key for encryption is 3 that is subtracted to ASCII value
 
-      cout << "\nDecrypted string: " << str << endl;
+      std::cout << "\nDecrypted string: " << str << std::endl;
       break;
 
       default:
-         cout << "\nInvalid Input !!!\n";
+         std::cout << "\nInvalid Input !!!\n";
    }
    return 0;
 }
